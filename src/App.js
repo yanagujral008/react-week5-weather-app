a7026aa9637dc61246dfa73d6a09f47ee7b66f2e
Mikicanad <mmmcf5290@gmail.com>
2025-02-02 11:42:33 -0800

    added the GitHub repo URL

--- src/App.js
+++ src/App.js
@@ -4,7 +4,13 @@ import './App.css';
 export default function App() {
   return (
     <div className="App">
-      Weather App
+      <h1>Weather App</h1>
+      <footer>
+        This project was coded by <a href="https://tranquil-tarsier-93eb74.netlify.app/" target="_blank" rel="noreferrer">Miki.I</a> and is{" "}
+      <a href="https://github.com/Mikicanad/react-week5-weather-app" target="_blank" rel="noreferrer">
+       open-sourced on GitHub
+      </a>
+      </footer>
     </div>
   );
 }
