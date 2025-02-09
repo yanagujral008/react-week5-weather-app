7e59e56d105e5b299f5c74bf7e4ff293bb08f4c4
Mikicanad <mmmcf5290@gmail.com>
2025-02-09 15:41:24 -0800

    Created WeatherForecast compornent

--- src/Weather.js
+++ src/Weather.js
@@ -1,6 +1,8 @@
 import React, { useState } from "react";
 import WeatherInfo from "./WeatherInfo";
+import WeatherForecast from"./WeatherForecast";
 import axios from "axios";
+
 
 import "./Weather.css";
 export default function Weather(props) {
@@ -51,7 +53,7 @@ export default function Weather(props) {
                 </div>
                 </form>
                 <WeatherInfo data={weatherData}/>
-                
+                <WeatherForecast />
             </div>
             );
         } else {
