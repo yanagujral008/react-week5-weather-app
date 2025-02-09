32b4844de3fe7a50bfa29edb01c659103b0a3737
Mikicanad <mmmcf5290@gmail.com>
2025-02-09 13:19:08 -0800

    created new compornent and can display different unit

--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -1,6 +1,7 @@
 import React from "react";
 import FormattedDate from "./FormattedDate";
 import WeatherIcon from "./WeatherIcon";
+import WeatherTemperature from "./WeatherTemperature";
 
 export default function WeatherInfo(props) {
     
@@ -14,15 +15,12 @@ export default function WeatherInfo(props) {
                 <div className="row mt-3">
                     <div className="col-6">
                         <div className="clearfix">
-        
                             <div className="float-left"> 
-                           
                             <WeatherIcon code={props.data.icon} size={64} />
                             </div>
                             
                             <div className="float-left">
-                               <span className="temperature">{Math.round(props.data.temperature)}</span>
-                               <span className="unit">°C</span>
+                            <WeatherTemperature celsius={props.data.temperature}/>
                             </div>
                         </div>
                     </div>
