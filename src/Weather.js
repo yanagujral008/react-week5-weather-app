d4bbcae76bb51672bc563a4b5193f50fa9f62e70
yanagujral008 <139848691+yanagujral008@users.noreply.github.com>
2025-02-16 21:35:44 +0530

    react weather app

--- src/Weather.js
+++ src/Weather.js
@@ -55,8 +55,8 @@ export default function Weather(props) {
                 <WeatherInfo data={weatherData}/>
                 <WeatherForecast coordinates={weatherData.coordinates}/>
                 <footer>
-                    This project was coded by <a href="https://tranquil-tarsier-93eb74.netlify.app/" target="_blank" rel="noopener noreferrer">Miki.I</a> and is{" "}
-                    <a href="https://github.com/Mikicanad/react-week5-weather-app" target="_blank" rel="noopener noreferrer">
+                    This project was coded by <a href="https://tranquil-tarsier-93eb74.netlify.app/" target="_blank" rel="noopener noreferrer">Yana Gujral</a> and is{" "}
+                    <a href="https://github.com/yanagujral008" target="_blank" rel="noopener noreferrer">
                     open-sourced on GitHub
                     </a> and <a href="https://weatherappweek5mk.netlify.app/" target="_blank" rel="noopener noreferrer">hosted on Netlify</a>
                 </footer>
