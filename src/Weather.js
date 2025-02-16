dbf161fde8e5a5c4e5cbcf0e322354c4e3e06290
Mikicanad <mmmcf5290@gmail.com>
2025-02-15 20:26:18 -0800

    fix some style

--- src/Weather.js
+++ src/Weather.js
@@ -45,7 +45,7 @@ export default function Weather(props) {
                 <form onSubmit={handleSubmit}>
                 <div className="row">
                     <div className="col-9">
-                       <input type="search" placeholder="Enter a city.." className="form-control" autoFocus="on" onChange={handleCityChange}/>
+                       <input type="search" placeholder="Enter a city.." className="form-control search-input" autoFocus="on" onChange={handleCityChange}/>
                     </div>
                     <div className="col-3">
                        <input type="submit" value="Search" className="btn btn-primary w-100" />
